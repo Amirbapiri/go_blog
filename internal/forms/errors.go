f600436065f182c26a115289c80430aedf7af781
amirbapiri <amirbapiri@gmail.com>
2021-08-28 23:11:44 +0430

    create related functions related to form errors

--- internal/forms/errors.go
+++ internal/forms/errors.go
@@ -1,3 +1,17 @@
 package forms
 
-type errors map[string]string
+type errors map[string][]string
+
+//Add adds new error related to a specific field in the form
+func (e errors) Add(field, errorMessage string) {
+	e[field] = append(e[field], errorMessage)
+}
+
+//Get returns the first error message of a specific field in the form
+func (e errors) Get(field string) string {
+	errorField := e[field]
+	if len(errorField) == 0 {
+		return ""
+	}
+	return errorField[0]
+}
